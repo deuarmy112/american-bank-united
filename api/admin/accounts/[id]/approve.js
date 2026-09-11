dde61a5aabb218647194cf18a7a529c4badd24fe
Developer <dev@example.com>
2026-09-11 23:25:21 +0300

    Fix admin account approval routing

---
+++ api/admin/accounts/[id]/approve.js
@@ -0,0 +1,8 @@
+const apiHandler = require('../../../[...path].js');
+
+module.exports = (req, res) => {
+    const requestPath = req.url.split('?')[0].replace(/^\/api\/?/, '');
+    const accountId = requestPath.split('/').filter(Boolean).slice(-2, -1)[0];
+    req.url = `/api/admin/accounts/${accountId}/approve`;
+    return apiHandler(req, res);
+};
