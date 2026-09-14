bf18018b75fa05508dce7c0dedab87e9150cda86
Developer <dev@example.com>
2026-09-14 15:10:04 +0300

    Fix Vercel API route deployment

--- api/accounts/lookup.js
+++
@@ -1,6 +0,0 @@
-const apiHandler = require('../../api/[...path]');
-
-module.exports = async (req, res) => {
-    req.url = `/api/accounts/lookup${req.url.includes('?') ? req.url.slice(req.url.indexOf('?')) : ''}`;
-    return apiHandler(req, res);
-};
--- api/transactions/transfer.js
+++
@@ -1,6 +0,0 @@
-const apiHandler = require('../../api/[...path]');
-
-module.exports = async (req, res) => {
-    req.url = `/api/transactions/transfer${req.url.includes('?') ? req.url.slice(req.url.indexOf('?')) : ''}`;
-    return apiHandler(req, res);
-};
