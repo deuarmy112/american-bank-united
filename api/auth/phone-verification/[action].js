89a7604ca81ff140f8ddd8aaad35ce36592e6fa0
Developer <dev@example.com>
2026-09-26 12:08:15 +0300

    hbjnkm

--- api/auth/phone-verification/[action].js
+++
@@ -1,8 +0,0 @@
-const apiHandler = require('../../../api/[...path].js');
-
-module.exports = (req, res) => {
-    const rawAction = req.query.action || req.url.split('?')[0].split('/').pop();
-    const action = Array.isArray(rawAction) ? rawAction.join('/') : String(rawAction || '');
-    req.url = `/api/auth/phone-verification/${action}`;
-    return apiHandler(req, res);
-};
